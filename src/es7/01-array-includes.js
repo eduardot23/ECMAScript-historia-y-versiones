a6b5ad019cbf92ef7e40e5b8fc485ec092d3caf3
eduardot23 <eduardojeda.t23@gmail.com>
2022-12-14 12:38:27 -0600

    Object entoires y values

--- src/es7/01-array-includes.js
+++ src/es7/01-array-includes.js
@@ -3,4 +3,4 @@ let numbers = [1,2,3,4,5,6,7,8,9,10]
 console.log(numbers.includes(8))
 
 const list = ["Eduardo", "Ana", "Oscar"]
-console.log(list.includes("eduardo"));
+console.log(list.includes("eduardo"))
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,2 @@
+const countries = {MX: "Mexico", CO: "Colombia", CH: "Chile", PE: "Peru"}
+console.log(Object.values(countries));
